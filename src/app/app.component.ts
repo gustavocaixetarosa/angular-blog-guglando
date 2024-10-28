38d8fad15b08b01907580182d3ca252079e74d70
Gustavo <gstvcaixeta@gmail.com>
2024-10-28 13:21:41 -0300

    att menu title

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MenuBarComponent } from "./components/menu-bar/menu-bar.component";
+import { MenuTitleComponent } from './components/menu-title/menu-title.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MenuBarComponent],
+  imports: [RouterOutlet, MenuBarComponent, MenuTitleComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
