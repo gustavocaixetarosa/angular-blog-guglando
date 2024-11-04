38bb15e10656015fb70e6e34e74c2c1dcc0c4bb3
Gustavo <gstvcaixeta@gmail.com>
2024-11-04 18:18:31 -0300

    Adicionando Input properties

--- src/app/components/big-card/big-card.component.ts
+++ src/app/components/big-card/big-card.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-big-card',
@@ -8,5 +8,12 @@ import { Component } from '@angular/core';
   styleUrl: './big-card.component.scss'
 })
 export class BigCardComponent {
+  @Input()
+  photoCover: string = "https://media.istockphoto.com/id/1409329028/pt/vetorial/no-picture-available-placeholder-thumbnail-icon-illustration-design.jpg?s=2048x2048&w=is&k=20&c=99fISV8MCqK-5ZphBqT-W50eTgq5UMBu0vteQ-rE-Do= ";
 
+  @Input()
+  cardTitle: string = "Teste de text interpolation";
+
+  @Input()
+  cardDescription: string = "Gosto bastante de trabalhar com Angular";
 }
--- src/app/components/small-card/small-card.component.ts
+++ src/app/components/small-card/small-card.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-small-card',
@@ -8,5 +8,9 @@ import { Component } from '@angular/core';
   styleUrl: './small-card.component.scss'
 })
 export class SmallCardComponent {
+  @Input()
+  photoCover: string = "https://media.istockphoto.com/id/1409329028/pt/vetorial/no-picture-available-placeholder-thumbnail-icon-illustration-design.jpg?s=2048x2048&w=is&k=20&c=99fISV8MCqK-5ZphBqT-W50eTgq5UMBu0vteQ-rE-Do= "
 
+  @Input()
+  cardTitle: string = "Vamos ver"
 }
